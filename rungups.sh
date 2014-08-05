c43627491a0634fff0e93f540352f1eea6a81287
Brandon Myers <bdmyers@cs.washington.edu>
2014-08-05 12:03:10 -0700

    script example to run gups

---
+++ rungups.sh
@@ -0,0 +1,2 @@
+java -cp core/target/scala-2.10/classes/:`/Users/brandon/escience/spark/spark-1.0.1-bin-hadoop2/bin/compute-classpath.sh` org.apache.spark.network.Gups 0 ~/escience/spark/hosts 1 &
+java -cp core/target/scala-2.10/classes/:`/Users/brandon/escience/spark/spark-1.0.1-bin-hadoop2/bin/compute-classpath.sh` org.apache.spark.network.Gups 1 ~/escience/spark/hosts 1 &
